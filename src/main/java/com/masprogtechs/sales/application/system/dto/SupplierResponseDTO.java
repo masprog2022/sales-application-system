c785830102b411f0f3a7cf8cc9bf36e69fd75ad3
masprog2022 <masprog2022@gmail.com>
2023-08-25 09:48:38 +0100

    :sparkles: feat: DTO SupplierResponseDTO para retornar apenas o nome do id e name do Supplier

--- src/main/java/com/masprogtechs/sales/application/system/dto/SupplierResponseDTO.java
+++ src/main/java/com/masprogtechs/sales/application/system/dto/SupplierResponseDTO.java
@@ -1,28 +1,20 @@
 package com.masprogtechs.sales.application.system.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.Column;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
-public class SupplierDTO {
+public class SupplierResponseDTO {
 
     private Long id;
     private String name;
-    @JsonProperty("registeredBy")
-    private UserDTO registeredBy;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
 
-    public SupplierDTO(String name, UserDTO registeredBy, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    public SupplierResponseDTO(String name) {
+
         this.name = name;
-        this.registeredBy = registeredBy;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
     }
 }
