da98ee8b879a6dd838502719d58af1b8bc7b8b9b
masprog2022 <masprog2022@gmail.com>
2023-08-25 09:52:47 +0100

    :sparkles: feat: DTO CategoryResponseDTO para retornar apenas o id e o name da Category

--- src/main/java/com/masprogtechs/sales/application/system/dto/CategoryResponseDTO.java
+++ src/main/java/com/masprogtechs/sales/application/system/dto/CategoryResponseDTO.java
@@ -1,34 +1,20 @@
 package com.masprogtechs.sales.application.system.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.masprogtechs.sales.application.system.domain.entities.User;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
-public class CategoryDTO {
+public class CategoryResponseDTO {
 
     private Long id;
     private String name;
-    private String description;
-    @JsonProperty("registeredBy")
-    private UserDTO registeredBy;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
 
-    public CategoryDTO(Long id, String name, String description, UserDTO registeredBy, LocalDateTime createdAt, LocalDateTime updatedAt) {
-        this.id = id;
+    public CategoryResponseDTO(String name) {
+
         this.name = name;
-        this.description = description;
-        this.registeredBy = registeredBy;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
     }
 }
