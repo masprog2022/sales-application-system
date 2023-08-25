00b5c828abe90db08ff03d6b2fde2419e82d1319
masprog2022 <masprog2022@gmail.com>
2023-08-25 09:16:44 +0100

    :sparkles: feat: criando metódo mapToReduceDTO para retornar um User reduzido

--- src/main/java/com/masprogtechs/sales/application/system/services/UserService.java
+++ src/main/java/com/masprogtechs/sales/application/system/services/UserService.java
@@ -3,6 +3,7 @@ package com.masprogtechs.sales.application.system.services;
 import com.masprogtechs.sales.application.system.domain.entities.User;
 import com.masprogtechs.sales.application.system.domain.repositories.UserRepository;
 import com.masprogtechs.sales.application.system.dto.UserDTO;
+import com.masprogtechs.sales.application.system.dto.UserReducedDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -41,5 +42,15 @@ public class UserService {
     public Page<UserDTO> listUersWithPagination(Pageable pageable){
         Page<User> userPage = userRepository.findAll(pageable);
         return userPage.map(user -> modelMapper.map(user, UserDTO.class));
+    }
+
+    public UserReducedDTO mapToReducedDTO(User user) {
+        UserReducedDTO reducedDTO = new UserReducedDTO();
+        reducedDTO.setName(user.getName());
+        reducedDTO.setUsername(user.getUsername());
+        reducedDTO.setEmail(user.getEmail());
+        reducedDTO.setPhone(user.getPhone());
+
+        return reducedDTO;
     }
 }
