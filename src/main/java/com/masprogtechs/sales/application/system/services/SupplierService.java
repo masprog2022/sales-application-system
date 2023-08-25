aff16ac28686926d09473e047c4341f7d1d0a370
masprog2022 <masprog2022@gmail.com>
2023-08-25 09:18:11 +0100

    :sparkles: feat: criando metódo registerService em SupplierService

--- src/main/java/com/masprogtechs/sales/application/system/services/SupplierService.java
+++ src/main/java/com/masprogtechs/sales/application/system/services/SupplierService.java
@@ -1,2 +1,55 @@
-package com.masprogtechs.sales.application.system.services;public class SupplierService {
+package com.masprogtechs.sales.application.system.services;
+
+import com.masprogtechs.sales.application.system.domain.entities.Supplier;
+import com.masprogtechs.sales.application.system.domain.entities.User;
+import com.masprogtechs.sales.application.system.domain.repositories.SupplierRepository;
+import com.masprogtechs.sales.application.system.domain.repositories.UserRepository;
+import com.masprogtechs.sales.application.system.dto.SupplierDTO;
+import com.masprogtechs.sales.application.system.exception.UnauthorizedAccessException;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class SupplierService {
+
+        @Autowired
+        private ModelMapper modelMapper;
+
+        @Autowired
+        private SupplierRepository supplierRepository;
+
+        @Autowired
+        private UserRepository userRepository;
+
+
+        public SupplierDTO registerSupplier(SupplierDTO supplierDTO) {
+            if (SecurityContextHolder.getContext().getAuthentication().isAuthenticated()) {
+
+                User registeredBy = userRepository.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+
+                List<String> allowedRoles = Arrays.asList("ADMIN", "OPERATOR");
+
+                if (registeredBy != null && allowedRoles.contains(registeredBy.getRole().name())) {
+                    Supplier supplier = modelMapper.map(supplierDTO, Supplier.class);
+                    supplier.setRegisteredBy(registeredBy);
+                    supplier.setCreatedAt(LocalDateTime.now());
+                    supplier.setUpdatedAt(LocalDateTime.now());
+
+                    Supplier savedSupplier = supplierRepository.save(supplier);
+
+                    return modelMapper.map(savedSupplier, SupplierDTO.class);
+                } else {
+                    throw new UnauthorizedAccessException("User is not authorized to register a product.");
+                }
+            } else {
+                throw new AuthenticationCredentialsNotFoundException("User is not authenticated.");
+            }
+        }
 }
