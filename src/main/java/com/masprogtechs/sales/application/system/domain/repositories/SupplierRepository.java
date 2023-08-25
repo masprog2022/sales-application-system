31094973b4223b1f7a39f4df008a3443658837b2
masprog2022 <masprog2022@gmail.com>
2023-08-25 09:55:41 +0100

    :sparkles: feat: Repositorio SupplierRepository

--- src/main/java/com/masprogtechs/sales/application/system/domain/repositories/SupplierRepository.java
+++ src/main/java/com/masprogtechs/sales/application/system/domain/repositories/SupplierRepository.java
@@ -1,2 +1,7 @@
-package com.masprogtechs.sales.application.system.domain.repositories;public class SupplierRepository {
+package com.masprogtechs.sales.application.system.domain.repositories;
+
+import com.masprogtechs.sales.application.system.domain.entities.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SupplierRepository extends JpaRepository<Supplier, Long> {
 }
