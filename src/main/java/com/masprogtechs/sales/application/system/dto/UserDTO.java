c38d9a93af2752bf28d3c1142431b6e1765d7a1a
masprog2022 <masprog2022@gmail.com>
2023-08-25 09:33:49 +0100

    :boom: fix: ajustes no UserDTO

--- src/main/java/com/masprogtechs/sales/application/system/dto/UserDTO.java
+++ src/main/java/com/masprogtechs/sales/application/system/dto/UserDTO.java
@@ -19,9 +19,7 @@ public class UserDTO {
     private String username;
     private String email;
     private String phone;
-    @JsonIgnore
     private String password;
-    @JsonIgnore
     private Role role;
 
     private LocalDateTime createdAt;
@@ -38,10 +36,4 @@ public class UserDTO {
         this.role = role;
     }
 
-    public UserDTO(String name, String username, String email, String phone) {
-        this.name = name;
-        this.username = username;
-        this.email = email;
-        this.phone = phone;
-    }
 }
