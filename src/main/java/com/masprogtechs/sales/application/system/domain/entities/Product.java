f736dfc227b3687ec04d4ab330d3383edcb0f7f6
masprog2022 <masprog2022@gmail.com>
2023-08-25 09:59:24 +0100

    :sparkles: feat: Entidade Product

--- src/main/java/com/masprogtechs/sales/application/system/domain/entities/Product.java
+++ src/main/java/com/masprogtechs/sales/application/system/domain/entities/Product.java
@@ -1,2 +1,66 @@
-package com.masprogtechs.sales.application.system.domain.entities;public class Product {
+package com.masprogtechs.sales.application.system.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_product")
+@Data
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "supplier_id")
+    private Supplier supplier;
+
+    @ManyToOne
+    @JoinColumn(name = "registered_by")
+    private User registeredBy;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    public Product(Long id, String name, String description, Category category,
+                   Supplier supplier, User registeredBy, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.category = category;
+        this.supplier = supplier;
+        this.registeredBy = registeredBy;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    @PrePersist
+    protected void prePersist(){
+        if(this.createdAt == null)
+            createdAt = LocalDateTime.now();
+        if(this.updatedAt == null)
+            updatedAt = LocalDateTime.now();
+    }
+    @PreUpdate
+    protected void preUpdate(){
+        this.updatedAt = LocalDateTime.now();
+    }
 }
