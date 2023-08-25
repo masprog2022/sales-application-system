801549935957f74528b3aee5a4c69618f65d9f29
masprog2022 <masprog2022@gmail.com>
2023-08-25 09:31:44 +0100

    :sparkles: feat: DTO para retornar user com poucos campos

--- src/main/java/com/masprogtechs/sales/application/system/dto/UserReducedDTO.java
+++ src/main/java/com/masprogtechs/sales/application/system/dto/UserReducedDTO.java
@@ -4,23 +4,19 @@ import com.masprogtechs.sales.application.system.domain.enums.Role;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 @Data
 @NoArgsConstructor
-public class UserResponseDTO {
+public class UserReducedDTO {
 
     private String name;
     private String username;
     private String email;
     private String phone;
-    private Role role;
 
-    public UserResponseDTO(String name, String username, String email, String phone, Role role) {
+    public UserReducedDTO(String name, String username, String email, String phone) {
         this.name = name;
         this.username = username;
         this.email = email;
         this.phone = phone;
-        this.role = role;
     }
 }
