0fd8ad0e6dccbb400630574b1d0b97cc127a2ec2
masprog2022 <masprog2022@gmail.com>
2023-08-25 09:56:05 +0100

    :sparkles: feat: Repositorio ProductRepository

--- src/main/java/com/masprogtechs/sales/application/system/domain/repositories/ProductRepository.java
+++ src/main/java/com/masprogtechs/sales/application/system/domain/repositories/ProductRepository.java
@@ -1,2 +1,9 @@
-package com.masprogtechs.sales.application.system.domain.repositories;public class ProductRepository {
+package com.masprogtechs.sales.application.system.domain.repositories;
+
+
+import com.masprogtechs.sales.application.system.domain.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
 }
