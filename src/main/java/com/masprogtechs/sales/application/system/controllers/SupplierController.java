19ff84267a7715c6aef5299521b6c2e54558fc6c
masprog2022 <masprog2022@gmail.com>
2023-08-25 10:03:05 +0100

    :sparkles: feat: adicionando o metódo registerSupplier no SupplierController

--- src/main/java/com/masprogtechs/sales/application/system/controllers/SupplierController.java
+++ src/main/java/com/masprogtechs/sales/application/system/controllers/SupplierController.java
@@ -1,2 +1,44 @@
-package com.masprogtechs.sales.application.system.controllers;public class SupplierController {
+package com.masprogtechs.sales.application.system.controllers;
+
+import com.masprogtechs.sales.application.system.dto.CategoryDTO;
+import com.masprogtechs.sales.application.system.dto.SupplierDTO;
+import com.masprogtechs.sales.application.system.dto.UserDTO;
+import com.masprogtechs.sales.application.system.services.SupplierService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/supplier")
+@Tag(name = "Fornecedor", description = "Endpoints para gerenciar Fornecedores")
+public class SupplierController {
+
+    @Autowired
+    private SupplierService supplierService;
+
+    @PostMapping
+    @Operation(summary = "Salvar um fornecdor", description = "Salvar um fornecedor",
+            tags = {"Fornecedor"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = UserDTO.class))
+                    ),
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            })
+    public ResponseEntity<SupplierDTO> registerCategory(@RequestBody SupplierDTO supplierDTO) {
+        SupplierDTO savedSupplierDTO = supplierService.registerSupplier(supplierDTO);
+        return ResponseEntity.ok(savedSupplierDTO);
+    }
 }
