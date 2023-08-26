c660106420cc8f6556cf1871cea4ab6ed6c434d8
masprog2022 <masprog2022@gmail.com>
2023-08-26 23:15:51 +0100

    :sparkles: feat: deserializer data para permitir inserir data no formato dd/MM/yyyy

--- src/main/java/com/masprogtechs/sales/application/system/deserializer/LocalDateTimeDeserializer.java
+++ src/main/java/com/masprogtechs/sales/application/system/deserializer/LocalDateTimeDeserializer.java
@@ -1,2 +1,22 @@
-package com.masprogtechs.sales.application.system.deserializer;public class LocalDateTimeDeserializer {
+package com.masprogtechs.sales.application.system.deserializer;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime> {
+
+    private static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    @Override
+    public LocalDateTime deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JacksonException {
+        String dateString = p.getText();
+        LocalDate localDate = LocalDate.parse(dateString, dateFormatter);
+        return localDate.atStartOfDay();
+    }
 }
