a7bb17b958419e260b0fa7801071fd8bcbe52e4c
masprog2022 <masprog2022@gmail.com>
2023-08-26 22:14:51 +0100

    :sparkles: feat: adicionando repositorio stockRepository

--- src/main/java/com/masprogtechs/sales/application/system/domain/repositories/StockRepository.java
+++ src/main/java/com/masprogtechs/sales/application/system/domain/repositories/StockRepository.java
@@ -1,2 +1,9 @@
-package com.masprogtechs.sales.application.system.domain.repositories;public interface StockRepository {
+package com.masprogtechs.sales.application.system.domain.repositories;
+
+import com.masprogtechs.sales.application.system.domain.entities.Stock;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StockRepository extends JpaRepository<Stock, Long> {
 }
