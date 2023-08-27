52ed435129f7744f16efae701aff3f238ff1d963
masprog2022 <masprog2022@gmail.com>
2023-08-27 13:28:58 +0100

    :sparkles: feat: criando entidade Cash

--- src/main/java/com/masprogtechs/sales/application/system/domain/entities/Cash.java
+++ src/main/java/com/masprogtechs/sales/application/system/domain/entities/Cash.java
@@ -1,13 +1,15 @@
 package com.masprogtechs.sales.application.system.domain.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_cash")
@@ -15,14 +17,28 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 public class Cash {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private BigDecimal openingBalance;
     private BigDecimal closingBalance;
     private BigDecimal salesTotal;
 
+    @OneToMany(mappedBy = "cash", cascade = CascadeType.ALL)
+    private List<Sale> sales = new ArrayList<>(); // Vendas registradas durante o período do caixa
+
     @ManyToOne
     @JoinColumn(name = "registered_by")
     private User registeredBy;
+
+    @CreatedDate
+    @Column(name = "opening_date", updatable = false)
+    private LocalDateTime openingDate;
+
+    @LastModifiedDate
+    @Column(name = "closing_date")
+    private LocalDateTime closingDate;
+
 
 
 }
