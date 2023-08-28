dd99e94ab2ae55a43bc1c4a0862fab52a0084e0e
masprog2022 <masprog2022@gmail.com>
2023-08-29 00:13:45 +0100

    :sparkles: feat: adicionando CashRepository

--- src/main/java/com/masprogtechs/sales/application/system/domain/repositories/CashRepository.java
+++ src/main/java/com/masprogtechs/sales/application/system/domain/repositories/CashRepository.java
@@ -1,2 +1,7 @@
-package com.masprogtechs.sales.application.system.domain.repositories;public interface CashRepository {
+package com.masprogtechs.sales.application.system.domain.repositories;
+
+import com.masprogtechs.sales.application.system.domain.entities.Cash;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CashRepository extends JpaRepository<Cash, Long> {
 }
