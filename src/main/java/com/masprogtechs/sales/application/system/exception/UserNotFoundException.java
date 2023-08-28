78d2ba1a67240921dbe34134e66f543a6218ebfe
masprog2022 <masprog2022@gmail.com>
2023-08-29 00:11:48 +0100

    :sparkles: feat: criando o metodo UserNotFoundException para tratamentos de erros

--- src/main/java/com/masprogtechs/sales/application/system/exception/UserNotFoundException.java
+++ src/main/java/com/masprogtechs/sales/application/system/exception/UserNotFoundException.java
@@ -1,2 +1,7 @@
-package com.masprogtechs.sales.application.system.exception;public class UserNotFoundException {
+package com.masprogtechs.sales.application.system.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
 }
