ea8ea5a7ccea2c4ca34e2e525b75fc9703e3c5ff
masprog2022 <masprog2022@gmail.com>
2023-09-04 15:05:45 +0100

    :sparkles: feat: adicionando o metódo findByIdAndClosingDateIsNull no CashRepository

--- src/main/java/com/masprogtechs/sales/application/system/domain/repositories/CashRepository.java
+++ src/main/java/com/masprogtechs/sales/application/system/domain/repositories/CashRepository.java
@@ -3,8 +3,15 @@ package com.masprogtechs.sales.application.system.domain.repositories;
 import com.masprogtechs.sales.application.system.domain.entities.Cash;
 import com.masprogtechs.sales.application.system.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface CashRepository extends JpaRepository<Cash, Long> {
 
     boolean existsByRegisteredByAndClosingDateIsNull(User registeredBy);
+    @Query("SELECT c FROM Cash c WHERE c.id = :cashIdParam AND c.closingDate IS NULL")
+    Optional<Cash> findByIdAndClosingDateIsNull(@Param("cashIdParam") Long cashId);
+
 }
