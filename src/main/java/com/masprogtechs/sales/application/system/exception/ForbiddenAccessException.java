037fa6daaad6ea5f48b03964cd324ba498ab8fcb
masprog2022 <masprog2022@gmail.com>
2023-09-05 14:49:30 +0100

    :sparkles: feat: adicionando ForbiddenAccessException

--- src/main/java/com/masprogtechs/sales/application/system/exception/ForbiddenAccessException.java
+++ src/main/java/com/masprogtechs/sales/application/system/exception/ForbiddenAccessException.java
@@ -1,2 +1,7 @@
-package com.masprogtechs.sales.application.system.exception;public class ForbiddenAccessException {
+package com.masprogtechs.sales.application.system.exception;
+
+public class ForbiddenAccessException extends RuntimeException {
+    public ForbiddenAccessException(String message) {
+        super(message);
+    }
 }
