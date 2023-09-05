3d76d19f0b70073f37f26c634662d50f72905ee1
masprog2022 <masprog2022@gmail.com>
2023-09-05 14:45:44 +0100

    :sparkles: feat: exception para validar a existencia de um username

--- src/main/java/com/masprogtechs/sales/application/system/exception/UsernameExistsException.java
+++ src/main/java/com/masprogtechs/sales/application/system/exception/UsernameExistsException.java
@@ -1,2 +1,7 @@
-package com.masprogtechs.sales.application.system.exception;public class UsernameExistsException {
+package com.masprogtechs.sales.application.system.exception;
+
+public class UsernameExistsException extends RuntimeException{
+    public UsernameExistsException(String message) {
+        super(message);
+    }
 }
