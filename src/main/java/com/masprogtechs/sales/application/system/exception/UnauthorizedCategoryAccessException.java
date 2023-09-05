ede686a408e491ff994fbc8af32805ca858eda21
masprog2022 <masprog2022@gmail.com>
2023-09-05 14:46:19 +0100

    :sparkles: feat: exception para validar autorização a uma category

--- src/main/java/com/masprogtechs/sales/application/system/exception/UnauthorizedCategoryAccessException.java
+++ src/main/java/com/masprogtechs/sales/application/system/exception/UnauthorizedCategoryAccessException.java
@@ -1,2 +1,7 @@
-package com.masprogtechs.sales.application.system.exception;public class UnauthorizedCategoryAccessException {
+package com.masprogtechs.sales.application.system.exception;
+
+public class UnauthorizedCategoryAccessException extends RuntimeException {
+    public UnauthorizedCategoryAccessException(String message) {
+        super(message);
+    }
 }
