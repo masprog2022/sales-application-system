7a90c070ca92623d8339f134c24277b181b6563f
masprog2022 <masprog2022@gmail.com>
2023-09-07 16:41:25 +0100

    :sparkles: Adicionando o metódo deleteBySaleId no SaleItemRepository

--- src/main/java/com/masprogtechs/sales/application/system/domain/repositories/SaleItemRepository.java
+++ src/main/java/com/masprogtechs/sales/application/system/domain/repositories/SaleItemRepository.java
@@ -1,2 +1,11 @@
-package com.masprogtechs.sales.application.system.domain.repositories;public class SaleItemRepository {
+package com.masprogtechs.sales.application.system.domain.repositories;
+
+import com.masprogtechs.sales.application.system.domain.entities.SaleItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SaleItemRepository extends JpaRepository<SaleItem, Long> {
+    List<SaleItem> findBySaleId(Long saleId);
+    void deleteBySaleId(Long saleId);
 }
