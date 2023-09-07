e14778d8d56de2e8f0f1e76999544648d5623943
masprog2022 <masprog2022@gmail.com>
2023-09-07 12:34:55 +0100

    :sparkles: adicionando o findByregisteredBy no repositorio SaleRepository

--- src/main/java/com/masprogtechs/sales/application/system/domain/repositories/SaleRepository.java
+++ src/main/java/com/masprogtechs/sales/application/system/domain/repositories/SaleRepository.java
@@ -1,8 +1,14 @@
 package com.masprogtechs.sales.application.system.domain.repositories;
 
 import com.masprogtechs.sales.application.system.domain.entities.Sale;
+import com.masprogtechs.sales.application.system.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+    // Método para buscar vendas por operador atribuído
+    List<Sale> findByregisteredBy(User operator);
 
 }
